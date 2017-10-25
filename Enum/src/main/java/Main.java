26ae652fbc75e4f4075f4dd6df2902f8378bb70c
EliGarcia <garciaelisha93@yahoo.com>
2017-10-25 16:31:37 -0500

    for each month, display

--- Enum/src/main/java/Main.java
+++ Enum/src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args){
 
+        for( Months m: Months.values() ) {
+            System.out.println(m.getEnglishName());
+        }
+
     }
 
 }
