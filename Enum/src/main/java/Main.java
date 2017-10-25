f838c388d28a1d006cd90422e7b1f930ff387151
EliGarcia <garciaelisha93@yahoo.com>
2017-10-25 16:32:46 -0500

    let user choose a month

--- Enum/src/main/java/Main.java
+++ Enum/src/main/java/Main.java
@@ -1,5 +1,8 @@
 import common.Months;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
@@ -7,6 +10,14 @@ public class Main {
         for( Months m: Months.values() ) {
             System.out.println(m.getEnglishName());
         }
+
+        System.out.println("Please choose a month:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayList<String> months = new ArrayList<String>();
 
     }
 
