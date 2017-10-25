34ea6bd068935f5f4f7e4372820d4499073f1415
EliGarcia <garciaelisha93@yahoo.com>
2017-10-25 16:29:55 -0500

    added motnhs. made object for names and made a getter

--- Enum/src/main/java/common/Months.java
+++ Enum/src/main/java/common/Months.java
@@ -1,6 +1,29 @@
 package common;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-public class Months {
+public enum Months {
+    JANUARY ("January"),
+    FEBRUARY ("February"),
+    MARCH ("March"),
+    APRIL ("April"),
+    MAY ("May"),
+    JUNE ("June"),
+    JULY ("July"),
+    AUGUST ("August"),
+    SEPTEMBER ("September"),
+    OCTOBER ("October"),
+    NOVEMBER ("November"),
+    DECEMBER ("December");
 
 
+    private String englishName;
+
+    Months(String englishName) {
+        this.englishName = englishName;
+    }
+
+    @JsonValue
+    public String getEnglishName() {
+        return englishName;
+    }
 }
